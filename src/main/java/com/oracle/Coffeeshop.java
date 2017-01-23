fa38613cac0887fe9b4111cf046b6402b19b3ed8
Gerald Venzl <gerald.venzl@gmail.com>
2017-01-23 14:33:38 -0800

    Initial commit

---
+++ src/main/java/com/oracle/Coffeeshop.java
@@ -0,0 +1,33 @@
+package com.oracle;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.X509TrustManager;
+
+public class Coffeeshop {
+
+	private static class DefaultTrustManager implements X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+    }
+	
+	public static void main(String[] args) throws Exception {
+		new Coffeeshop().run();
+
+	}
+	
+	public void run() {
+		
+	}
+
+}
