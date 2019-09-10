8d05780c9851438d9f328a705d5da112055dfda8
Gerald Venzl <gerald.venzl@gmail.com>
2019-09-10 11:31:17 -0700

    Update ORDERS table

--- src/main/resources/setup/1_ORDERS.sql
+++ src/main/resources/setup/1_ORDERS.sql
@@ -1,26 +1,26 @@
-DROP TABLE SALES;
+DROP TABLE ORDERS;
 
 CREATE TABLE ORDERS (
-  order_detail  BLOB,
-  city          VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.city'  RETURNING VARCHAR2(25))),
-  state         VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.state' RETURNING VARCHAR2(25))),
-  sale_amount   NUMBER       AS (JSON_VALUE(order_detail, '$.sales_amount'   RETURNING NUMBER))
+  order_details  BLOB,
+  city           VARCHAR2(25) AS (JSON_VALUE(order_details, '$.location.city'  RETURNING VARCHAR2(25))),
+  state          VARCHAR2(25) AS (JSON_VALUE(order_details, '$.location.state' RETURNING VARCHAR2(25))),
+  sale_amount    NUMBER       AS (JSON_VALUE(order_details, '$.sales_amount'   RETURNING NUMBER))
 )
-LOB (order_detail) STORE AS SECUREFILE
+LOB (order_details) STORE AS SECUREFILE
 (
   ENABLE STORAGE IN ROW
 );
 
-DROP TABLE SALES_HISTORY;
+DROP TABLE ORDERS_HISTORY;
 
 CREATE TABLE ORDERS_HISTORY (
-  order_detail  BLOB,
-  order_date    DATE         AS (TO_DATE(JSON_VALUE(order_detail, '$.date'), 'YYYY-MM-DD HH24:MI:SS')),
-  city          VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.city'  RETURNING VARCHAR2(25))),
-  state         VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.state' RETURNING VARCHAR2(25))),
-  sale_amount   NUMBER       AS (JSON_VALUE(order_detail, '$.sales_amount'   RETURNING NUMBER))
+  order_details  BLOB,
+  order_date     DATE         AS (TO_DATE(JSON_VALUE(order_details, '$.date'), 'YYYY-MM-DD HH24:MI:SS')),
+  city           VARCHAR2(25) AS (JSON_VALUE(order_details, '$.location.city'  RETURNING VARCHAR2(25))),
+  state          VARCHAR2(25) AS (JSON_VALUE(order_details, '$.location.state' RETURNING VARCHAR2(25))),
+  sale_amount    NUMBER       AS (JSON_VALUE(order_details, '$.sales_amount'   RETURNING NUMBER))
 )
-LOB (order_detail) STORE AS SECUREFILE
+LOB (order_details) STORE AS SECUREFILE
 (
   ENABLE STORAGE IN ROW
 );
