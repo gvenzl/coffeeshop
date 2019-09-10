ad35dd680ee7a5fbacd4f2a8d3f44387bc5872f5
Gerald Venzl <gerald.venzl@gmail.com>
2019-09-10 11:04:42 -0700

    Update ORDERS table

--- src/main/resources/setup/1_ORDERS.sql
+++ src/main/resources/setup/1_ORDERS.sql
@@ -1,12 +1,12 @@
 DROP TABLE SALES;
 
 CREATE TABLE ORDERS (
-  order         BLOB,
-  city         VARCHAR2(25) AS (JSON_VALUE(sale, '$.location.city'  RETURNING VARCHAR2(25))),
-  state        VARCHAR2(25) AS (JSON_VALUE(sale, '$.location.state' RETURNING VARCHAR2(25))),
-  sale_amount  NUMBER       AS (JSON_VALUE(sale, '$.sales_amount'   RETURNING NUMBER))
+  order_detail  BLOB,
+  city          VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.city'  RETURNING VARCHAR2(25))),
+  state         VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.state' RETURNING VARCHAR2(25))),
+  sale_amount   NUMBER       AS (JSON_VALUE(order_detail, '$.sales_amount'   RETURNING NUMBER))
 )
-LOB (order) STORE AS SECUREFILE
+LOB (order_detail) STORE AS SECUREFILE
 (
   ENABLE STORAGE IN ROW
 );
@@ -14,13 +14,13 @@ LOB (order) STORE AS SECUREFILE
 DROP TABLE SALES_HISTORY;
 
 CREATE TABLE ORDERS_HISTORY (
-  order         BLOB,
-  order_date    DATE         AS (TO_DATE(JSON_VALUE(sale, '$.date'), 'YYYY-MM-DD HH24:MI:SS')),
-  city         VARCHAR2(25) AS (JSON_VALUE(sale, '$.location.city'  RETURNING VARCHAR2(25))),
-  state        VARCHAR2(25) AS (JSON_VALUE(sale, '$.location.state' RETURNING VARCHAR2(25))),
-  sale_amount  NUMBER       AS (JSON_VALUE(sale, '$.sales_amount'   RETURNING NUMBER))
+  order_detail  BLOB,
+  order_date    DATE         AS (TO_DATE(JSON_VALUE(order_detail, '$.date'), 'YYYY-MM-DD HH24:MI:SS')),
+  city          VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.city'  RETURNING VARCHAR2(25))),
+  state         VARCHAR2(25) AS (JSON_VALUE(order_detail, '$.location.state' RETURNING VARCHAR2(25))),
+  sale_amount   NUMBER       AS (JSON_VALUE(order_detail, '$.sales_amount'   RETURNING NUMBER))
 )
-LOB (order) STORE AS SECUREFILE
+LOB (order_detail) STORE AS SECUREFILE
 (
   ENABLE STORAGE IN ROW
 );
